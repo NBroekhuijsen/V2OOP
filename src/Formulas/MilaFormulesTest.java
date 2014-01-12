d0de6e3a3b85d39f350295bf4b967d9ac5fd3656
Julian <Julian@Julian-HP>
2014-01-12 17:59:46 +0100

    tussendoortje

--- src/Formulas/MilaFormulesTest.java
+++ src/Formulas/MilaFormulesTest.java
@@ -22,13 +22,22 @@ public class MilaFormulesTest {
 	testarli1.add(2);
 	testarli1.add(5);
 	testarli1.add(5);
+	
+	ArrayList<Object> testlijst1 = new ArrayList<Object>();
+	testlijst1.add("blabla");
+	testlijst1.add(2);
+	
+	ArrayList<Object> testlijst2 = new ArrayList<Object>();
+	testlijst1.add("blabla");
 		
 		assertEquals("10.0", MilaFormules.sum(testarli1));
 		assertEquals("16.0", MilaFormules.sum(testarli2));
 		assertNotSame("100.0", MilaFormules.sum(testarli1));
 		assertNotSame("15.0", MilaFormules.sum(testarli2));
-		//		Test for Exception?
+		assertTrue(MilaFormules.sum(teslijst1).equals("2"));
+		assertTrue(MilaFormules.sum(teslijst2).equals("0"));
 	}
+	
 	
 	@Test
 	public void testSumif() {
